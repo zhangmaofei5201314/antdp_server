c8e3858c57453c7e81c7ef98b44c5c62b47bc705
Black <1024169266@qq.com>
2020-08-28 11:24:14 +0800

    2020年8月28日 11:23:28，quartz,删除任务计划，启动任务，停止任务。

--- src/main/java/com/donbala/quartzManagement/dao/QuartzMapper.java
+++ src/main/java/com/donbala/quartzManagement/dao/QuartzMapper.java
@@ -4,6 +4,7 @@ import com.donbala.quartzManagement.model.Quartz;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface QuartzMapper {
@@ -18,6 +19,23 @@ public interface QuartzMapper {
     int insertJobPlanDef(Quartz qz);
     // 记录作业计划参数
     int insertJobPlanParam(Quartz qz);
+    // 删除任务
+    int deleteJobPlanDef(String jobPlanCode);
+    // 删除任务参数
+    int deleteJobPlanParam(String jobPlanCode);
+    // 变更作业状态为启动
+    int startJobPlanDef(String jobPlanCode);
+    // 变更作业状态为停止
+    int stopJobPlanDef(String jobPlanCode);
+    // 查询作业内容，job类名
+    Quartz selectJobPlanClass(String jobPlanCode);
+    // 查询计划运行状态
+    Quartz selectJobPlanState(String jobPlanCode);
+    // 计划回显
+    List<Quartz> selectReturnView(Quartz qz);
+    // 参数回显
+    List<Quartz> selectParamByCode(Quartz qz);
+
 
 //    int selectJobPlanCount(Quartz qz);
 //    List<Quartz> selectJobName();
@@ -25,26 +43,16 @@ public interface QuartzMapper {
 //    // 查询作业参数
 
 
-//    // 删除任务
-//    int deleteJobPlanDef(Map<String, String> map);
-//    // 删除任务参数
-//    int deleteJobPlanParam(Map<String, String> map);
-//    // 变更作业状态为停止
-//    int stopJobPlanDef(Map<String, String> map);
-//    // 变更作业状态为启动
-//    int startJobPlanDef(Map<String, String> map);
-//    // 查询作业内容-主要是查询job类名
-//    Quartz selectStopJobPlan(Map<String, String> map);
-//    // 计划回显
-//    List<Quartz> selectReturnView(Quartz qz);
-//    // 参数回显
-//    List<Quartz> selectParamByCode(Quartz qz);
+
+
+
+
+
 //    // 删除作业参数
 //    int deletePlanParam(Quartz qz);
 //    // 修改作业任务
 //    int updateJobPlan(Quartz qz);
-//    // 查询计划运行状态
-//    Quartz selectJobPlanState(String jobPlanCode);
+
 //    // 更新任务执行日志
 //    void updateJobRunLog(Map<String, String> map);
 //    // 查询运行日志的流水号，以后应该不用，暂时保留日后优化
